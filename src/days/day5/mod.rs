5e5048c468dbb16657bc0f69b301c9524fe2e93b
Gregory Presser <gpress2222@gmail.com>
2022-12-05 01:23:59 -0500

    day 5

---
+++ src/days/day5/mod.rs
@@ -0,0 +1,98 @@
+use std::collections::LinkedList;
+
+fn parse_instruction(instruction: &str) -> Vec<usize> {
+    instruction
+        .split(" ")
+        .flat_map(str::parse::<usize>)
+        .map(|i| i - 1)
+        .collect::<Vec<_>>()
+}
+
+fn build_original(original: &str) -> Vec<LinkedList<char>> {
+    let len = original
+        .lines()
+        .rev()
+        .next()
+        .unwrap()
+        .trim_end()
+        .chars()
+        .rev()
+        .next()
+        .unwrap()
+        .to_digit(10)
+        .unwrap();
+
+    let mut v: Vec<LinkedList<char>> = vec![LinkedList::new(); len as usize];
+    for crates in original.lines() {
+        let mut counter = 0;
+        for (i, cr) in crates.split(" ").enumerate() {
+            if cr.as_bytes().len() != 3 {
+                if i % 4 == 0 {
+                    counter += 1
+                }
+                continue;
+            }
+            v[counter % len as usize].push_back(cr.as_bytes()[1] as char);
+            counter += 1;
+        }
+    }
+    return v;
+}
+
+pub fn part_1(input: &str) -> String {
+    let (original, rest) = input.split_once("\n\n").unwrap();
+    let mut v = build_original(original);
+    for instruction in rest.lines() {
+        let instruction = parse_instruction(instruction);
+        for _ in 0..=instruction[0] {
+            let val = v[instruction[1]].pop_front().unwrap();
+            v[instruction[2]].push_front(val);
+        }
+    }
+    v.iter_mut()
+        .map(|a| a.pop_front().unwrap())
+        .fold("".to_string(), |acc, c| format!("{}{}", acc, c))
+}
+
+pub fn part_2(input: &str) -> String {
+    let (original, rest) = input.split_once("\n\n").unwrap();
+    let mut v = build_original(original);
+    for instruction in rest.lines() {
+        let instruction = parse_instruction(instruction);
+        for val in (0..=instruction[0])
+            .flat_map(|_| v[instruction[1]].pop_front())
+            .collect::<Vec<_>>()
+            .iter()
+            .rev()
+        {
+            v[instruction[2]].push_front(*val);
+        }
+    }
+
+    v.iter_mut()
+        .map(|a| a.pop_front().unwrap())
+        .fold("".to_string(), |acc, c| format!("{}{}", acc, c))
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    const INPUT: &'static str = "    [D]
+[N] [C]
+[Z] [M] [P]
+ 1   2   3
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2";
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(part_1(INPUT), "CMZ");
+    }
+    #[test]
+    fn test_part_2() {
+        assert_eq!(part_2(INPUT), "MCD");
+    }
+}
--- src/utill/chunk.rs
+++ src/utill/chunk.rs
@@ -2,10 +2,10 @@ pub struct Chunk<I, const A: usize> {
     iter: I,
 }
 
-impl<I, T: Copy, const A: usize> Chunk<I, A>
+impl<I, T, const A: usize> Chunk<I, A>
 where
     I: Iterator<Item = T>,
-    T: Copy + Default,
+    T: Default,
 {
     fn new(iter: I) -> Self {
         Self { iter }
@@ -15,25 +15,25 @@ where
 impl<I, T, const A: usize> Iterator for Chunk<I, A>
 where
     I: Iterator<Item = T>,
-    T: Copy + Default,
+    T: Default + Copy,
 {
     type Item = [T; A];
     fn next(&mut self) -> Option<Self::Item> {
-        let mut arr: Self::Item = [T::default(); A];
+        let mut arr = [T::default(); A];
         for v in arr.iter_mut() {
-            *v = self.iter.next()?
+            *v = self.iter.next()?;
         }
         Some(arr)
     }
 }
 
-pub trait ChunkIterator<T: Copy + Default, const A: usize>: Iterator<Item = T> + Sized {
-    fn chunk(self) -> Chunk<Self, A> {
+pub trait ChunkIterator<T: Default>: Iterator<Item = T> + Sized {
+    fn chunk<const A: usize>(self) -> Chunk<Self, A> {
         Chunk::new(self)
     }
 }
 
-impl<T: Copy + Default, I: Iterator<Item = T>, const A: usize> ChunkIterator<T, A> for I {}
+impl<T: Default, I: Iterator<Item = T>> ChunkIterator<T> for I {}
 
 #[cfg(test)]
 mod test {
@@ -50,5 +50,22 @@ mod test {
             .reduce(|acc, v| format!("{}|{}", acc, v))
             .unwrap();
         assert_eq!(answer.to_string(), a);
+    }
+
+    #[derive(Default, Clone, Copy)]
+    struct Foo {
+        a: i32,
+        b: i32,
+    }
+
+    #[test]
+    fn test_chunk_non_mut() {
+        let lst = [
+            Foo::default(),
+            Foo::default(),
+            Foo::default(),
+            Foo::default(),
+        ];
+        // let a = lst.iter().chunk::<3>().collect::<Vec<_>>();
     }
 }
